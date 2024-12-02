5b2c4665bf03399d5a3d2ed761c852c7a523a240
hk221 <108066429+hk221@users.noreply.github.com>
2024-12-02 16:37:18 +0000

    team forming working

--- agents/Team2Agent.go
+++ agents/Team2Agent.go
@@ -39,42 +39,31 @@ func (t2a *Team2Agent) SetTrustScore(id uuid.UUID) {
 // Overall function to update one agents trust score for other agents
 // (can either implement like this or call functions underneath during each event)
 func (t2a *Team2Agent) UpdateTrustScore(agentID uuid.UUID, eventType string, strikeCount int) {
-	// auditContributionResult := t2a.server.GetContributionAuditResult(agentID) //fix
-	// auditWithdrawalResult := t2a.server.GetWithdrawalAuditResult(agentID) //fix
-	auditWithdrawalResult := false
-	auditContributionResult := false
+	auditContributionResult := t2a.server.GetTeam(t2a.GetID()).TeamAoA.GetContributionAuditResult(agentID) //fix
+	auditWithdrawalResult := t2a.server.GetTeam(t2a.GetID()).TeamAoA.GetWithdrawalAuditResult(agentID)
 	switch eventType {
 	case "strike":
-		if auditContributionResult {
-			t2a.ApplyStrike(agentID) // from helper function
+		if auditContributionResult || auditWithdrawalResult{
+			t2a.ApplyStrike(agentID) 
 		}
-		if auditWithdrawalResult {
-			t2a.ApplyStrike(agentID) // from helper function
-		}
-
 	case "notAudited":
-		// If the target agent was not audited
-		t2a.ApplyNotAudited(agentID)
-
-	case "auditOther":
-		// If the target agent audited another agent
-		t2a.ApplyAuditOther(agentID)
-
+		if !auditContributionResult || !auditWithdrawalResult{
+			// If the target agent was not audited
+			t2a.ApplyNotAudited(agentID)
+		}
 	default:
 		fmt.Println("Invalid event type")
 	}
 }
 
-// update when not cooperating based on strikes
+// update when not cooperating based on strikes 
 func (t2a *Team2Agent) ApplyStrike(agentID uuid.UUID) {
 	if t2a.trustScore == nil {
 		t2a.SetTrustScore(agentID)
 	}
-
 	t2a.strikeCount[agentID]++ // Increment the strike count for this agent
 	var penalty int
 	strikeCount := t2a.strikeCount[agentID]
-
 	if strikeCount == 1 {
 		penalty = 10
 	}
@@ -87,25 +76,9 @@ func (t2a *Team2Agent) ApplyStrike(agentID uuid.UUID) {
 		// should never reach this point
 		penalty = 40
 	}
-
 	// Update trust score based on strike count
 	t2a.trustScore[agentID] -= penalty
 }
-
-// update trust score if agent kicked out of other team -->>(can't implement yet?)
-// func (t2a *Team2Agent) ApplyKickFromTeam(agentID uuid.UUID) {
-//     // Update trust score based on being kicked from another team
-//     t2a.trustScore[agentID] -= 5
-// }
-
-// update trust score based on audit vote (maybe exclude this as shouldn't know other agents votes?)
-// func (t2a *Team2Agent) ApplyAuditVote(agentID uuid.UUID) {
-// 	if t2a.trustScore == nil {
-// 		t2a.SetTrustScore(agentID)
-// 	}
-// 	// Update trust score based on audit vote
-// 	t2a.trustScore[agentID] -= 5
-// }
 
 // update if agent not audited for that round
 func (t2a *Team2Agent) ApplyNotAudited(agentID uuid.UUID) {
@@ -116,14 +89,6 @@ func (t2a *Team2Agent) ApplyNotAudited(agentID uuid.UUID) {
 	t2a.trustScore[agentID] += 2
 }
 
-// update if chooses to audit another agent
-func (t2a *Team2Agent) ApplyAuditOther(agentID uuid.UUID) {
-	if t2a.trustScore == nil {
-		t2a.SetTrustScore(agentID)
-	}
-	// Update trust score based on auditing another agent
-	t2a.trustScore[agentID] += 2
-}
 
 // Section 2:
 
@@ -155,11 +120,6 @@ func (t2a *Team2Agent) DecideTeamForming(agentInfoList []common.ExposedAgentInfo
 
 		// // Check if we're a leader and they're not
 		// if t2a.rank {
-		// 	// Leaders are more selective and only accept followers with high trust
-		// 	if trustScore >= trustThreshold {
-		// 		selectedAgents = append(selectedAgents, agentUUID)
-		// 	}
-		// 	continue
 		// }
 
 		// Choose agent with highest trust score
@@ -169,74 +129,58 @@ func (t2a *Team2Agent) DecideTeamForming(agentInfoList []common.ExposedAgentInfo
 		}
 
 	}
+
 	lenInviteList := len(invitationList)
 	chosenAgent := invitationList[lenInviteList-1]
 
 	return []uuid.UUID{chosenAgent}
 }
 
-// func (t2a *Team2Agent) DecideTeamForming(agentInfoList []common.ExposedAgentInfo) []uuid.UUID {
-// 	invitationList := []uuid.UUID{}
-// 	for _, agentInfo := range agentInfoList {
-// 		// exclude the agent itself
-// 		if agentInfo.AgentUUID == t2a.GetID() {
-// 			continue
-// 		}
-// 		if agentInfo.AgentTeamID == (uuid.UUID{}) {
-// 			invitationList = append(invitationList, agentInfo.AgentUUID)
-// 		}
-// 	}
-
-// 	// random choice from the invitation list
-// 	rand.Shuffle(len(invitationList), func(i, j int) { invitationList[i], invitationList[j] = invitationList[j], invitationList[i] })
-// 	if len(invitationList) == 0 {
-// 		return []uuid.UUID{}
-// 	}
-// 	chosenAgent := invitationList[0]
-
-// 	// Return a slice containing the chosen agent
-// 	return []uuid.UUID{chosenAgent}
-// }
-
 // ----- 2.2 Decision to stick -----
 
 func (t2a *Team2Agent) StickorAgain() {}
 
-func (t2a *Team2Agent) GetActualContribution(instance common.IExtendedAgent) int {
-	if t2a.HasTeam() {
-		contribution := instance.DecideContribution()
-		if t2a.verboseLevel > 6 {
-			fmt.Printf("%s is contributing %d to the common pool and thinks the common pool size is %d\n", t2a.GetID(), contribution, t2a.server.GetTeam(t2a.GetID()).GetCommonPool())
+func (t2a *Team2Agent) StickOrAgainFor(agentId uuid.UUID, accumulatedScore int, prevRoll int) int {
+	return 0
+}
+
+// ----- 2.3 Decision to cheat / not cooperate -----
+
+func (t2a *Team2Agent) DecideContribution() int {
+	// MVP: contribute exactly as defined in AoA
+	if t2a.server.GetTeam(t2a.GetID()).TeamAoA != nil {
+		aoaExpectedContribution := t2a.server.GetTeam(t2a.GetID()).TeamAoA.GetExpectedContribution(t2a.GetID(), t2a.GetTrueScore())
+		// double check if score in agent is sufficient (this should be handled by AoA though)
+		if t2a.GetTrueScore() < aoaExpectedContribution {
+			return t2a.GetTrueScore() // give all score if less than expected
 		}
-		return contribution
+		return aoaExpectedContribution
 	} else {
 		if t2a.verboseLevel > 6 {
-			fmt.Printf("%s has no team, skipping contribution\n", t2a.GetID())
+			// should not happen!
+			fmt.Printf("[WARNING] Agent %s has no AoA, contributing 0\n", t2a.GetID())
 		}
 		return 0
 	}
 }
 
-// make withdrawal from common pool
-func (t2a *Team2Agent) GetActualWithdrawal(instance common.IExtendedAgent) int {
-	currentPool := t2a.server.GetTeam(t2a.GetID()).GetCommonPool()
-	withdrawal := instance.DecideWithdrawal()
-	fmt.Printf("%s is withdrawing %d from the common pool of size %d\n", t2a.GetID(), withdrawal, currentPool)
-	return withdrawal
+// Decide the withdrawal amount based on AoA and current pool size
+func (t2a *Team2Agent) DecideWithdrawal() int {
+	if t2a.server.GetTeam(t2a.GetID()).TeamAoA != nil {
+		// double check if score in agent is sufficient (this should be handled by AoA though)
+		commonPool := t2a.server.GetTeam(t2a.GetID()).GetCommonPool()
+		aoaExpectedWithdrawal := t2a.server.GetTeam(t2a.GetID()).TeamAoA.GetExpectedWithdrawal(t2a.GetID(), t2a.GetTrueScore(), commonPool)
+		if commonPool < aoaExpectedWithdrawal {
+			return commonPool
+		}
+		return aoaExpectedWithdrawal
+	} else {
+		if t2a.verboseLevel > 6 {
+			fmt.Printf("[WARNING] Agent %s has no AoA, withdrawing 0\n", t2a.GetID())
+		}
+		return 0
+	}
 }
-
-// func (t2a *Team2Agent) DecideWithdrawal() int {
-//     // Get the current points in the common pool
-//     currentPoints := t2a.server.GetTeam(t2a.GetID()).GetCommonPool()
-//     // Get common pool size
-// 	commonPoolSize := t2a.server.GetTeam(t2a.GetID()).GetCommonPool()
-//     // Get the expected withdrawal from the AOA
-//     expectedWithdrawal := t2a.server.GetTeam(t2a.teamID).TeamAoA.(*common.Team2AoA).GetExpectedWithdrawal(t2a.GetID(), currentPoints, commonPoolSize)
-
-//     // If the expected withdrawal is more than current points, withdraw all current points
-//     return expectedWithdrawal
-// }
-// ----- 2.3 Decision to cheat / not cooperate
 
 // func (t2a *Team2Agent) DecideContribution() int {
 // 	// Get AoA expected contribution
@@ -339,9 +283,9 @@ func (t2a *Team2Agent) GetContributionAuditVote() common.Vote {
 	// 1: Setup
 
 	// experiment with these;
-	var auditThreshold int = 50   // decision to audit based on if an agents trust score is lower than this
-	var suspicionFactor int = 20  // how much we lower everyone's trust scores if there is a discrepancy.
-	var discrepancyThreshold = 40 // if discrepancy between stated and actual common pool is greater than this, lower trust scores.
+	auditThreshold := 50       // decision to audit based on if an agents trust score is lower than this
+	suspicionFactor := 20      // how much we lower everyone's trust scores if there is a discrepancy.
+	discrepancyThreshold := 40 // if discrepancy between stated and actual common pool is greater than this, lower trust scores.
 
 	// get list of uuids in our team
 	var agentsInTeam []uuid.UUID = t2a.server.GetAgentsInTeam(t2a.teamID)
@@ -400,9 +344,9 @@ func (t2a *Team2Agent) GetWithdrawalAuditVote() common.Vote {
 	// 1: Setup
 
 	// experiment with these;
-	var auditThreshold int = 50   // decision to audit based on if an agents trust score is lower than this
-	var suspicionFactor int = 20  // how much we lower everyone's trust scores if there is a discrepancy.
-	var discrepancyThreshold = 40 // if discrepancy between stated and actual common pool is greater than this, lower trust scores.
+	auditThreshold := 50       // decision to audit based on if an agents trust score is lower than this
+	suspicionFactor := 20      // how much we lower everyone's trust scores if there is a discrepancy.
+	discrepancyThreshold := 40 // if discrepancy between stated and actual common pool is greater than this, lower trust scores.
 
 	// get list of uuids in our team
 	var agentsInTeam []uuid.UUID = t2a.server.GetAgentsInTeam(t2a.teamID)
--- server/Team2Server.go
+++ server/Team2Server.go
@@ -2,16 +2,77 @@ package environmentServer
 
 import (
 	"fmt"
-	"github.com/google/uuid"
+	"log"
 	"math/rand"
+
+	"github.com/ADimoska/SOMASExtended/agents"
+	"github.com/ADimoska/SOMASExtended/common"
+	"github.com/google/uuid"
 )
+
+func (cs *EnvironmentServer) AoA2_gameLoop(teamId uuid.UUID) {
+	team := cs.Teams[teamId]
+	agentsInTeam := cs.GetAgentsInTeam(teamId)
+
+	team.TeamAoA.(*common.Team2AoA).RunAoAStuff()
+
+	for _, agentId := range agentsInTeam {
+		agent := cs.GetAgentMap()[agentId]
+		switch a := agent.(type) {
+		case *agents.Team2Agent:
+			a.SetTrustScore(agentId)
+		}
+	}
+}
 
 /*
  * In case the leader is caught cheating, they will be deposed,
  * this calls a vote for the leader among agents on the current team.
+ * TODO: convert this to a borda vote maybe? ->
+ * Would need ordered preference of votes
  */
-func (cs *EnvironmentServer) DeposeLeader() {
+func (cs *EnvironmentServer) DeposeLeader(teamId uuid.UUID) {
+	agentsInTeam := cs.GetAgentsInTeam(teamId)
+	if len(agentsInTeam) <= 0 {
+		log.Fatal("Team can't have non-positive agent count")
+	}
 
+	votes := make(map[uuid.UUID]int)
+	var maxVotes int
+	var candidates []uuid.UUID
+
+	for _, agentId := range agentsInTeam {
+		agent := cs.GetAgentMap()[agentId]
+		leaderVote := agent.(*agents.Team2Agent).GetLeaderVote()
+		votedFor := leaderVote.VotedForID
+
+		votes[votedFor]++
+		voteCount := votes[votedFor]
+
+		// In case of new maximum, create a new tie-break array
+		if voteCount > maxVotes {
+			maxVotes = voteCount
+			candidates = []uuid.UUID{votedFor}
+		} else if voteCount == maxVotes {
+			candidates = append(candidates, votedFor)
+			// Update the old tie-break array
+		}
+	}
+
+	// Handle tie by selecting randomly
+	var selectedLeader uuid.UUID
+	if len(candidates) > 1 {
+		selectedLeader = candidates[rand.Intn(len(candidates))]
+	} else if len(candidates) == 1 {
+		selectedLeader = candidates[0]
+	}
+
+	if len(candidates) == 0 {
+		log.Fatal("No candidate selected!")
+	}
+
+	team := cs.Teams[teamId]
+	team.TeamAoA.(*common.Team2AoA).SetLeader(selectedLeader)
 }
 
 /*
