fae038a7e5970b53b4a9e65885cedbb43154d162
Matthieu Bou <matthieubou@Matthieus-MacBook-Pro.local>
2024-12-11 12:22:29 +0000

    Updated Team3AoA with IRV voting system and strategy-based punishment

--- server/EnvironmentServer.go
+++ server/EnvironmentServer.go
@@ -595,26 +595,27 @@ func (cs *EnvironmentServer) allocateAoAs() {
 			// Update the team's strategy
 			switch preference {
 			case 1:
-				team.TeamAoA = common.CreateTeam3AoA()
-				team.TeamAoAID = 3
+				team.TeamAoA = common.CreateTeam1AoA(team)
+				team.TeamAoAID = 1
 			case 2:
-				team.TeamAoA = common.CreateTeam3AoA()
-				team.TeamAoAID = 3
+				team.TeamAoA = common.CreateTeam2AoA(team, uuid.Nil, 5)
+				team.TeamAoAID = 2
+				cs.ElectNewLeader(team.TeamID)
 			case 3:
 				team.TeamAoA = common.CreateTeam3AoA()
 				team.TeamAoAID = 3
 			case 4:
-				team.TeamAoA = common.CreateTeam3AoA()
-				team.TeamAoAID = 3
+				team.TeamAoA = common.CreateTeam4AoA(team)
+				team.TeamAoAID = 4
 			case 5:
-				team.TeamAoA = common.CreateTeam3AoA()
-				team.TeamAoAID = 3
+				team.TeamAoA = common.CreateTeam5AoA()
+				team.TeamAoAID = 5
 			case 6:
-				team.TeamAoA = common.CreateTeam3AoA()
-				team.TeamAoAID = 3
+				team.TeamAoA = common.CreateTeam6AoA()
+				team.TeamAoAID = 6
 			default:
-				team.TeamAoA = common.CreateTeam3AoA()
-				team.TeamAoAID = 3
+				team.TeamAoA = common.CreateFixedAoA(1)
+				team.TeamAoAID = 0
 			}
 
 			cs.Teams[team.TeamID] = team
